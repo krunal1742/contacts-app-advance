bda839aa89a7d038d41ec9278c172c0462b0eca3
Krunal1742 <krunal1742@gmail.com>
2025-04-27 11:29:35 +0530

    Changes

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="ui fixed menu headerPart">
+      <div className="ui container text center">
+        <h2>Contact Manager</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
